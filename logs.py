c271267a77bf5a9769a2bf4c9e89948318b98649
Andrey Stepanyuk <stepanyuk.aa@list.ru>
2020-08-09 10:23:16 +0600

    change logs for display time on search new number

--- logs.py
+++ logs.py
@@ -9,9 +9,16 @@ def add_log(text, time = False):
     file = connect_to_file()
     if time:
         file.write(str(datetime.now()) + "\t" + text + "\n")
-        print(str(datetime.now()) + "\t" + text + "\n")
+        print(str(datetime.now()) + "\t" + text)
     else:
         file.write(text + "\n")
-        print(text + "\n")
+        print(text)
 
     file.close()
+
+def mark_time():
+    return datetime.now()\
+
+def dif_time(mark1, mark2):
+    return mark2 - mark1
+
--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from check import *
 from logs import *
 
+mS = mark_time()
 add_log("Start")
+m1 = mark_time()
 for w in range(2,100000):
-    if checker(w): add_log(str(w) + ' is simple', True)
+    if checker(w):
+        add_log(str(dif_time(m1, mark_time())) + "\t" + str(w))
+        m1 = mark_time()
 
-add_log("Stop")
-
+add_log(str(dif_time(mS, mark_time())) + "\t Stop")
